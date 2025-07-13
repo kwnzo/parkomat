a3b49070f10d112811aeb26af7b3b6fdab9d8cdc
kwnzo <93256819+kwnzo@users.noreply.github.com>
2025-07-13 21:50:37 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -13,7 +13,6 @@ def create_app():
     login.init_app(app)
     login.login_view = 'auth.login'
 
-    # Регистрация Blueprint после создания app
     from .routes import main, auth
     app.register_blueprint(main)
     app.register_blueprint(auth, url_prefix='/auth')
@@ -21,22 +20,19 @@ def create_app():
     # Создание таблиц
     with app.app_context():
         db.create_all()
-        # Добавление тестовых данных (опционально)
         from .models import ParkingSpot, ParkingZone
         if not ParkingZone.query.first():
             zone = ParkingZone(name="Основная парковка", location="Главный вход")
             db.session.add(zone)
-            db.session.commit()  # Фиксируем зону сначала!
+            db.session.commit() 
 
-                # Создаем тестовые места, только если их нет
             if not ParkingSpot.query.first():
-                # Получаем созданную зону
                 zone = ParkingZone.query.first()
                 
                 for i in range(1, 11):
                     spot = ParkingSpot(
                         number=f"A-{i}",
-                        zone_id=zone.id,  # Теперь zone_id точно есть
+                        zone_id=zone.id,
                         is_available=True
                     )
                     db.session.add(spot)
